6eba6ba1f1bd033a9f2d0b3ca32b22a8db766fd0
ivanmacedonio778 <ivanmacedonio778@gmail.com>
2023-10-27 18:05:31 -0300

    feat: add card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import "./App.css";
+import { Card } from "./components/Card";
 import { useFetchGit } from "./hooks/useFetchGit";
 function App() {
-  const {data, error} = useFetchGit()
-
-  return <></>;
+  const { data, error } = useFetchGit();
+  console.log(data);
+  return (
+    <div className="app">
+      {data.map((commit) => (
+        <div className="cardContainer">
+          <Card></Card>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/Card.css";
+export const Card = () => {
+  return (
+    <>
+      <div class="card">
+        <label class="avatar"></label>
+        <label class="info">
+          <span class="info-1"></span>
+          <span class="info-2"></span>
+        </label>
+        <div class="content-1"></div>
+      </div>
+    </>
+  );
+};
