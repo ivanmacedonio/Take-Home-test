3b59386d9aec1ea03207071671d61c2afe65345b
ivanmacedonio778 <ivanmacedonio778@gmail.com>
2023-10-27 20:15:14 -0300

    feature: add responsiveness

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import me from "../assets/me.jpg";
 import "../styles/Card.css";
 export const Card = ({ img, title, author, message }) => {
   return (
     <>
       <div className="card">
         <label className="avatar">
-          <img src={me} />
+          <img src={img} />
         </label>
         <label className="info">
           <span className="info-1">{title}</span>
